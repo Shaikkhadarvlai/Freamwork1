1f260399f20f9b5fce18a8cdf53a51ea4d289219
Shaikkhadarvlai <khadarsk414@gmail.com>
2024-07-07 22:04:24 +0530

    Test purpose

---
+++ src/test/java/Testprac/prac1.java
@@ -0,0 +1,20 @@
+package Testprac;
+
+import java.util.Scanner;
+
+public class prac1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Input print First number= ");
+		int num1 = sc.nextInt();
+
+		System.out.println("Input print Second number= ");
+		int num2 = sc.nextInt();
+
+		System.out.println(num1 + "+" + num2 + "=" + (num1 + num2));
+
+	}
+
+}
