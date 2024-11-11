860b4397a425dadec371d86e38fc8bd29296e931
Shaik. Khadar vali <khadarsk414@gmail.com>
2024-11-11 11:34:38 +0530

    jenkins11-112024 second time

--- src/test/java/test1.java
+++ src/test/java/test1.java
@@ -2,7 +2,8 @@
 public class test1
 {
 	public static void main(String[] args) {
-		System.out.println("this for test porpuse");
+		System.out.println("this for test porpuse ");
+	     System.out.println("this is one more time agian test");
 	}
 
 }
