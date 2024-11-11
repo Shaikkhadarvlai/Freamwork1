d8abdb22449858c356f2c0406525b4b87e901886
Shaik. Khadar vali <khadarsk414@gmail.com>
2024-11-11 12:22:17 +0530

    jenkins thidrs time

---
+++ src/test/java/test2.java
@@ -0,0 +1,18 @@
+
+public class test2 
+{
+	public static void main(String[] args) {
+		int a=10;
+		int b=11;
+		int sum=a+b;
+		System.out.println(sum);
+		
+		int a1 =10;
+		int b1=20;
+		int minuse=a1-b1;
+		
+		System.out.println(minuse);
+	
+	}
+
+}
